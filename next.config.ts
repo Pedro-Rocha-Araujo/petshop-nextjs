16307b43cfcea6e3725f03b75e4f0d7412b3a6d3
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-19 08:45:25 -0300

    Configurações de doploy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   reactCompiler: true,
+  basePath: `/petshop-nextjs`,
+  assetPrefix: `/petshop-nextjs`,
 };
 
 export default nextConfig;
