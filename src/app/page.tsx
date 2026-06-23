f193c9d3c4b3d8d8e2c2ad04e1594b5cd4296b0a
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-23 11:55:05 -0300

    Informações de metadata para maior SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { Metadata } from "next"
+
 import Header from "@/components/header/page"
 import Curiosidades from "@/components/curiosidades/page";
 import Relatos from "@/components/relatos/page";
 import Planos from "@/components/planos/page";
 import Footer from "@/components/footer/page"
+
+export const metadata:Metadata = {
+  title: 'Petshop | Home',
+  description: 'Página home do petshop, onde são oferecidos os nossos serviços e informações para contato'
+}
 
 export default function Home() {
   return (
