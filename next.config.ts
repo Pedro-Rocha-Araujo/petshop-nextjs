85661f245ffc041d501d55efb29e365a83f4c77a
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-23 11:49:59 -0300

    Correção nos erros de deploy

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
   reactCompiler: true,
-  basePath: `/petshop-nextjs`,
-  assetPrefix: `/petshop-nextjs`,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
